6fe1751f45c49dd01e5df551c65113fec01aa7fe
NguyenPhiDu <nguyendu1249@gmail.com>
2022-04-18 22:03:19 +0700

    update the directory structure and create Launcher icon

---
+++ src/Utils/Color.js
@@ -0,0 +1,10 @@
+export const Colors = {
+    primary: "#E85626",
+    second: "#AF2D23",
+    third: "#C4C4C4",
+    fourth: "#606060",
+    fifth: "#F5F5F5",
+    sixth: "#E5E5E5",
+    seventh: "#FFFFFF",
+    eighth: "#000000"
+}
